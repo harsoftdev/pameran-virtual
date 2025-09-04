7048e85a5b8d161aa34bf70388d09a1025e8fe89
Muhammad Harisz Ryadi <harisz.ryadi17@gmail.com>
2025-09-04 18:38:53 +0700

    update async

--- main.js
+++ main.js
@@ -12,39 +12,37 @@ import { setupPlayButton } from "./modules/menu.js";
 import { clickHandling } from "./modules/clickHandling.js";
 import { createFurniture } from "./modules/furniture.js";
 
-let { camera, controls, renderer } = setupScene();
+(async () => {
+  let { camera, controls, renderer } = setupScene();
+  const textureLoader = new THREE.TextureLoader();
 
-const textureLoader = new THREE.TextureLoader();
+  const walls = createWalls(scene, textureLoader);
+  const floor = setupFloor(scene);
+  const furniture = createFurniture(scene);
+  const ceiling = createCeiling(scene, textureLoader);
+  const door = createDoor(scene, textureLoader);
+  const paintings = await createPaintings(scene, textureLoader);
 
-const walls = createWalls(scene, textureLoader);
-const floor = setupFloor(scene);
-const furniture = createFurniture(scene);
-const ceiling = createCeiling(scene, textureLoader);
-const door = createDoor(scene, textureLoader);
-const paintings = await createPaintings(scene, textureLoader);
+  createBoundingBoxes(walls);
+  createBoundingBoxes(paintings);
 
-createBoundingBoxes(walls);
-createBoundingBoxes(paintings);
+  addObjectsToScene(scene, paintings);
 
-addObjectsToScene(scene, paintings);
+  setupPlayButton(controls);
+  setupEventListeners(controls);
+  clickHandling(renderer, camera, paintings);
+  setupRendering(scene, camera, renderer, paintings, controls, walls);
 
-setupPlayButton(controls);
-
-setupEventListeners(controls);
-
-clickHandling(renderer, camera, paintings);
-
-setupRendering(scene, camera, renderer, paintings, controls, walls);
-
-document.addEventListener('pointerlockchange', () => {
+  document.addEventListener('pointerlockchange', () => {
     const infoElement = document.getElementById('painting-info');
     if (document.pointerLockElement) {
-        infoElement.classList.add('locked');
+      infoElement.classList.add('locked');
     } else {
-        infoElement.classList.remove('locked');
+      infoElement.classList.remove('locked');
     }
-});
+  });
 
-const loaderDiv = document.getElementById('loader');
-loaderDiv.style.opacity = 0;
-setTimeout(() => loaderDiv.style.display = 'none', 500);
+  const loaderDiv = document.getElementById('loader');
+  loaderDiv.style.opacity = 0;
+  setTimeout(() => loaderDiv.style.display = 'none', 500);
+})();
